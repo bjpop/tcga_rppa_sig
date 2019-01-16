571c3bf7de1abd365f210ab4e346e977350816de
Bernie Pope <bjpope@unimelb.edu.au>
2019-01-17 09:16:09 +1100

    Read in data

--- tcga_rppa_sig/tcga_rppa_sig.py
+++ tcga_rppa_sig/tcga_rppa_sig.py
@@ -11,19 +11,16 @@ variety of statistics, and then prints a summary of the statistics as output.
 '''
 
 from argparse import ArgumentParser
-from math import floor
+import csv
 import sys
 import logging
 import pkg_resources
-from Bio import SeqIO
 
 
 EXIT_FILE_IO_ERROR = 1
 EXIT_COMMAND_LINE_ERROR = 2
-EXIT_FASTA_FILE_ERROR = 3
-DEFAULT_MIN_LEN = 0
+EXIT_BAD_INPUT = 3
 DEFAULT_VERBOSE = False
-HEADER = 'FILENAME\tNUMSEQ\tTOTAL\tMIN\tAVG\tMAX'
 PROGRAM_NAME = "tcga_rppa_sig"
 
 
@@ -55,12 +52,17 @@ def parse_args():
     description = 'Read one or more FASTA files, compute simple stats for each file'
     parser = ArgumentParser(description=description)
     parser.add_argument(
-        '--minlen',
-        metavar='N',
-        type=int,
-        default=DEFAULT_MIN_LEN,
-        help='Minimum length sequence to include in stats (default {})'.format(
-            DEFAULT_MIN_LEN))
+        '--rppa',
+        metavar='FILE',
+        type=str,
+        required=True,
+        help='Filename of RPPA data')
+    parser.add_argument(
+        '--sigs',
+        metavar='FILE',
+        type=str,
+        required=True,
+        help='Filename of mutational signature data')
     parser.add_argument('--version',
                         action='version',
                         version='%(prog)s ' + PROGRAM_VERSION)
@@ -68,137 +70,7 @@ def parse_args():
                         metavar='LOG_FILE',
                         type=str,
                         help='record program progress in LOG_FILE')
-    parser.add_argument('fasta_files',
-                        nargs='*',
-                        metavar='FASTA_FILE',
-                        type=str,
-                        help='Input FASTA files')
     return parser.parse_args()
-
-
-class FastaStats(object):
-    '''Compute various statistics for a FASTA file:
-
-    num_seqs: the number of sequences in the file satisfying the minimum
-       length requirement (minlen_threshold).
-    num_bases: the total length of all the counted sequences.
-    min_len: the minimum length of the counted sequences.
-    max_len: the maximum length of the counted sequences.
-    average: the average length of the counted sequences rounded down
-       to an integer.
-    '''
-    #pylint: disable=too-many-arguments
-    def __init__(self,
-                 num_seqs=None,
-                 num_bases=None,
-                 min_len=None,
-                 max_len=None,
-                 average=None):
-        "Build an empty FastaStats object"
-        self.num_seqs = num_seqs
-        self.num_bases = num_bases
-        self.min_len = min_len
-        self.max_len = max_len
-        self.average = average
-
-    def __eq__(self, other):
-        "Two FastaStats objects are equal iff their attributes are equal"
-        if type(other) is type(self):
-            return self.__dict__ == other.__dict__
-        return False
-
-    def __repr__(self):
-        "Generate a printable representation of a FastaStats object"
-        return "FastaStats(num_seqs={}, num_bases={}, min_len={}, max_len={}, " \
-            "average={})".format(
-                self.num_seqs, self.num_bases, self.min_len, self.max_len,
-                self.average)
-
-    def from_file(self, fasta_file, minlen_threshold=DEFAULT_MIN_LEN):
-        '''Compute a FastaStats object from an input FASTA file.
-
-        Arguments:
-           fasta_file: an open file object for the FASTA file
-           minlen_threshold: the minimum length sequence to consider in
-              computing the statistics. Sequences in the input FASTA file
-              which have a length less than this value are ignored and not
-              considered in the resulting statistics.
-        Result:
-           A FastaStats object
-        '''
-        num_seqs = num_bases = 0
-        min_len = max_len = None
-        for seq in SeqIO.parse(fasta_file, "fasta"):
-            this_len = len(seq)
-            if this_len >= minlen_threshold:
-                if num_seqs == 0:
-                    min_len = max_len = this_len
-                else:
-                    min_len = min(this_len, min_len)
-                    max_len = max(this_len, max_len)
-                num_seqs += 1
-                num_bases += this_len
-        if num_seqs > 0:
-            self.average = int(floor(float(num_bases) / num_seqs))
-        else:
-            self.average = None
-        self.num_seqs = num_seqs
-        self.num_bases = num_bases
-        self.min_len = min_len
-        self.max_len = max_len
-        return self
-
-    def pretty(self, filename):
-        '''Generate a pretty printable representation of a FastaStats object
-        suitable for output of the program. The output is a tab-delimited
-        string containing the filename of the input FASTA file followed by
-        the attributes of the object. If 0 sequences were read from the FASTA
-        file then num_seqs and num_bases are output as 0, and min_len, average
-        and max_len are output as a dash "-".
-
-        Arguments:
-           filename: the name of the input FASTA file
-        Result:
-           A string suitable for pretty printed output
-        '''
-        if self.num_seqs > 0:
-            num_seqs = str(self.num_seqs)
-            num_bases = str(self.num_bases)
-            min_len = str(self.min_len)
-            average = str(self.average)
-            max_len = str(self.max_len)
-        else:
-            num_seqs = num_bases = "0"
-            min_len = average = max_len = "-"
-        return "\t".join([filename, num_seqs, num_bases, min_len, average,
-                          max_len])
-
-
-def process_files(options):
-    '''Compute and print FastaStats for each input FASTA file specified on the
-    command line. If no FASTA files are specified on the command line then
-    read from the standard input (stdin).
-
-    Arguments:
-       options: the command line options of the program
-    Result:
-       None
-    '''
-    if options.fasta_files:
-        for fasta_filename in options.fasta_files:
-            logging.info("Processing FASTA file from %s", fasta_filename)
-            try:
-                fasta_file = open(fasta_filename)
-            except IOError as exception:
-                exit_with_error(str(exception), EXIT_FILE_IO_ERROR)
-            else:
-                with fasta_file:
-                    stats = FastaStats().from_file(fasta_file, options.minlen)
-                    print(stats.pretty(fasta_filename))
-    else:
-        logging.info("Processing FASTA file from stdin")
-        stats = FastaStats().from_file(sys.stdin, options.minlen)
-        print(stats.pretty("stdin"))
 
 
 def init_logging(log_filename):
@@ -222,13 +94,81 @@ def init_logging(log_filename):
         logging.info('program started')
         logging.info('command line: %s', ' '.join(sys.argv))
 
+# Sample IDs look like:
+# TCGA-AO-A1KP-01A-21-A17I-20
+# we only use the first 12 chars
+def parse_rppa_sample_id(sample_id):
+    if len(sample_id) >= 12:
+        return sample_id[:12]
+    else:
+        exit_with_error("Cannot parse RPPA sample id: {}".format(sample_id), EXIT_BAD_INPUT)
+
+
+def read_rppa(rppa_filename):
+    # sample_id->cancer->sample_type->data
+    results = {}
+    rppa_sample_ids = set()
+    rppa_cancer_types = set()
+    rppa_sample_types = set()
+    with open(rppa_filename) as rppa_file:
+        reader = csv.DictReader(rppa_file)
+        for row in reader:
+            this_sample = parse_rppa_sample_id(row['Sample_ID'])
+            rppa_sample_ids.add(this_sample)
+            this_cancer = row['Cancer_Type']
+            rppa_cancer_types.add(this_cancer)
+            this_sample_type = row['Sample_Type']
+            rppa_sample_types.add(this_sample_type)
+            if this_sample not in results:
+                results[this_sample] = {}
+            if this_cancer not in results[this_sample]:
+                results[this_sample][this_cancer] = {}
+            results[this_sample][this_cancer][this_sample_type] = row
+    return rppa_sample_ids, rppa_cancer_types, rppa_sample_types, results
+
+
+# Sample IDs look like:
+# TCGA.OR.A5J1 
+# we join the fields to be consistent with the RPPA datah
+# TCGA-OR-A5J1
+# There are some non TCGA sample IDs in the data, we return them unchanged
+def parse_sig_sample_id(sample_id):
+    if sample_id.startswith('TCGA') and len(sample_id) == 12:
+        return sample_id.replace('.', '-')
+    else:
+        #logging.info("Non TCGA sample ID in signature data: {}".format(sample_id))
+        return sample_id 
+
+
+def read_sigs(sigs_filename):
+    # sample_id->data
+    results = {}
+    sigs_sample_ids = set()
+    with open(sigs_filename) as sigs_file:
+        reader = csv.DictReader(sigs_file, delimiter='\t')
+        for row in reader:
+            this_sample = parse_sig_sample_id(row['Tumor_Sample_Barcode'])
+            sigs_sample_ids.add(this_sample)
+    return sigs_sample_ids, results
+
+
+def join_data(rppa, sigs):
+    pass
+
+def get_correlations(rppa_sigs):
+    pass
 
 def main():
     "Orchestrate the execution of the program"
     options = parse_args()
     init_logging(options.log)
-    print(HEADER)
-    process_files(options)
+    rppa_sample_ids, rppa_cancer_types, rppa_sample_types, rppa = read_rppa(options.rppa)
+    sigs_sample_ids, sigs = read_sigs(options.sigs)
+    logging.info("Number of RRPA samples: {}".format(len(rppa_sample_ids)))
+    logging.info("Number of SIGS samples: {}".format(len(sigs_sample_ids)))
+    logging.info("Intersection of RPPA and SIGS samples: {}".format(len(sigs_sample_ids.intersection(rppa_sample_ids))))
+    rppa_sigs = join_data(rppa, sigs)
+    get_correlations(rppa_sigs)
 
 
 # If this script is run from the command line then call the main function.
