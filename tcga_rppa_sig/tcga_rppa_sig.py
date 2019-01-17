c0aebeb1c9329a40d163810758f09d0a1059821b
Bernie Pope <bjpope@unimelb.edu.au>
2019-01-17 11:46:42 +1100

    Join RPPA and Signature data on sample ID

--- tcga_rppa_sig/tcga_rppa_sig.py
+++ tcga_rppa_sig/tcga_rppa_sig.py
@@ -15,6 +15,7 @@ import csv
 import sys
 import logging
 import pkg_resources
+from collections import defaultdict
 
 
 EXIT_FILE_IO_ERROR = 1
@@ -63,6 +64,12 @@ def parse_args():
         type=str,
         required=True,
         help='Filename of mutational signature data')
+    parser.add_argument(
+        '--join',
+        metavar='FILE',
+        type=str,
+        required=False,
+        help='Options filename to output join of RPPA and SIG data')
     parser.add_argument('--version',
                         action='version',
                         version='%(prog)s ' + PROGRAM_VERSION)
@@ -103,28 +110,52 @@ def parse_rppa_sample_id(sample_id):
     else:
         exit_with_error("Cannot parse RPPA sample id: {}".format(sample_id), EXIT_BAD_INPUT)
 
+def cancer_types_per_sample(sample_rppa):
+    counts = [] 
+    for sample in sample_rppa:
+        num_cancers = len(sample_rppa[sample])
+        counts.append(num_cancers)
+    return min(counts), max(counts)
+
 
 def read_rppa(rppa_filename):
     # sample_id->cancer->sample_type->data
     results = {}
     rppa_sample_ids = set()
     rppa_cancer_types = set()
-    rppa_sample_types = set()
+    rppa_sample_types = defaultdict(int) 
+    num_missing_protein_values = 0
+    num_valid_protein_values = 0
     with open(rppa_filename) as rppa_file:
         reader = csv.DictReader(rppa_file)
+        fieldnames = reader.fieldnames
+        # fieldnames start with "Sample_ID,Cancer_Type,Sample_Type" then have protein names
+        non_protein_fields = "Sample_ID,Cancer_Type,Sample_Type".split(',')
+        protein_names = fieldnames[len(non_protein_fields):]
         for row in reader:
             this_sample = parse_rppa_sample_id(row['Sample_ID'])
             rppa_sample_ids.add(this_sample)
             this_cancer = row['Cancer_Type']
             rppa_cancer_types.add(this_cancer)
             this_sample_type = row['Sample_Type']
-            rppa_sample_types.add(this_sample_type)
+            rppa_sample_types[this_sample_type] += 1
             if this_sample not in results:
                 results[this_sample] = {}
             if this_cancer not in results[this_sample]:
                 results[this_sample][this_cancer] = {}
-            results[this_sample][this_cancer][this_sample_type] = row
-    return rppa_sample_ids, rppa_cancer_types, rppa_sample_types, results
+            if this_sample_type not in results[this_sample][this_cancer]:
+                results[this_sample][this_cancer][this_sample_type] = {}
+            for protein in protein_names:
+                try:
+                    this_protein_value = float(row[protein])
+                    num_valid_protein_values += 1
+                except:
+                    this_protein_value = None 
+                    num_missing_protein_values += 1
+                results[this_sample][this_cancer][this_sample_type][protein] = this_protein_value
+        logging.info("Number of missing protein values: {}".format(num_missing_protein_values))
+        logging.info("Number of valid protein values: {}".format(num_valid_protein_values))
+        return protein_names, rppa_sample_ids, rppa_cancer_types, rppa_sample_types, results
 
 
 # Sample IDs look like:
@@ -141,19 +172,76 @@ def parse_sig_sample_id(sample_id):
 
 
 def read_sigs(sigs_filename):
-    # sample_id->data
+    # sample_id->field->value
+    num_missing_sig_values = 0
+    num_valid_sig_values = 0
     results = {}
+    signature_associations = {}
     sigs_sample_ids = set()
     with open(sigs_filename) as sigs_file:
         reader = csv.DictReader(sigs_file, delimiter='\t')
         for row in reader:
             this_sample = parse_sig_sample_id(row['Tumor_Sample_Barcode'])
             sigs_sample_ids.add(this_sample)
-    return sigs_sample_ids, results
+            if this_sample not in results:
+                results[this_sample] = {}
+            this_signature = row['Signature']
+            this_association = row['Association']
+            if this_signature not in signature_associations:
+                signature_associations[this_signature] = this_association
+            try:
+                this_signature_value = float(row['Contribution'])
+                num_valid_sig_values += 1
+            except:
+                this_signature_value = None 
+                num_missing_sig_values += 1
+            if this_signature not in results[this_sample]:
+                results[this_sample][this_signature] = this_signature_value
+            else:
+                exit_with_error("Duplicate signature {} for sample {}".format(this_signature, this_sample))
+    logging.info("Number of missing signature values: {}".format(num_missing_sig_values))
+    logging.info("Number of valid signature values: {}".format(num_valid_sig_values))
+    return sigs_sample_ids, signature_associations, results
 
 
-def join_data(rppa, sigs):
-    pass
+def join_on_sample(rppa, sigs):
+    #results[this_sample][this_cancer][this_sample_type][protein] = this_protein_value
+    # sample -> fields -> value
+    results = {}
+    for sample, cancer_types in rppa.items():
+        if sample in sigs:
+            for cancer_type, sample_types in cancer_types.items():
+                for sample_type, proteins in sample_types.items():
+                    # Only consider primaries at the moment
+                    if sample_type == 'Primary':
+                        if sample not in results:
+                            results[sample] = {}
+                        else:
+                            exit_with_error("Duplicate sample in joined RPPA SIGs data: {}".format(sample))
+                        if cancer_type not in results[sample]:
+                            results[sample][cancer_type] = {'sigs': {}, 'proteins': {}}
+                        sig_values = sigs[sample]
+                        for s in sig_values:
+                            results[sample][cancer_type]['sigs'][s] = sig_values[s]
+                        for p in proteins:
+                            results[sample][cancer_type]['proteins'][p] = proteins[p]
+    return results
+
+def output_join(output_filename, join_data, cancer_types, sig_names, protein_names):
+    header = ["sample", "cancer"] + sig_names + protein_names
+    with open(output_filename, "w") as out_file:
+        print(",".join(header), file=out_file)
+        for sample, cancer_types in join_data.items():
+            for cancer_type in cancer_types:
+                this_sigs = cancer_types[cancer_type]['sigs']
+                this_proteins = cancer_types[cancer_type]['proteins']
+                output_row = [sample, cancer_type]
+                for sig in sig_names:
+                    output_row.append(str(this_sigs[sig]))
+                for protein in protein_names:
+                    output_row.append(str(this_proteins[protein]))
+                print(",".join(output_row), file=out_file)
+        
 
 def get_correlations(rppa_sigs):
     pass
@@ -162,13 +250,22 @@ def main():
     "Orchestrate the execution of the program"
     options = parse_args()
     init_logging(options.log)
-    rppa_sample_ids, rppa_cancer_types, rppa_sample_types, rppa = read_rppa(options.rppa)
-    sigs_sample_ids, sigs = read_sigs(options.sigs)
+    rppa_protein_names, rppa_sample_ids, rppa_cancer_types, rppa_sample_types, rppa = read_rppa(options.rppa)
+    min_cancer_types_per_sample, max_cancer_types_per_sample = cancer_types_per_sample(rppa)
+    logging.info("Minumum cancer types per sample: {}".format(min_cancer_types_per_sample))
+    logging.info("Maximum cancer types per sample: {}".format(max_cancer_types_per_sample))
     logging.info("Number of RRPA samples: {}".format(len(rppa_sample_ids)))
+    logging.info("RPPA sample types: {}".format(dict(rppa_sample_types)))
+    logging.info("RRPA protein names: {}".format(rppa_protein_names))
+    logging.info("Number of RRPA proteins: {}".format(len(rppa_protein_names)))
+    sigs_sample_ids, sigs_associations, sigs = read_sigs(options.sigs)
     logging.info("Number of SIGS samples: {}".format(len(sigs_sample_ids)))
     logging.info("Intersection of RPPA and SIGS samples: {}".format(len(sigs_sample_ids.intersection(rppa_sample_ids))))
-    rppa_sigs = join_data(rppa, sigs)
-    get_correlations(rppa_sigs)
+    join_data = join_on_sample(rppa, sigs)
+    if options.join:
+        sig_names = sorted(sigs_associations.keys())
+        output_join(options.join, join_data, rppa_cancer_types, sig_names, rppa_protein_names)
+    get_correlations(join_data)
 
 
 # If this script is run from the command line then call the main function.
